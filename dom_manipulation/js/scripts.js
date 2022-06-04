242d8be9c0e171c3ec7df4b668ac48242ef74725
guriaserrana <ap.of26@gmail.com>
2022-06-04 00:01:48 -0300

    DOM added

---
+++ dom_manipulation/js/scripts.js
@@ -0,0 +1,24 @@
+const list = document.querySelector('ul');
+    const input = document.querySelector('input');
+    const button = document.querySelector('button');
+
+    button.addEventListener('click', () => {
+        const entry = input.value;
+        input.value = '';
+
+        const bookChap = document.createElement('li');
+        const listText = document.createElement('span');
+        const newBtn = document.createElement('button');
+
+        bookChap.appendChild(listText);
+        listText.textContent = entry;
+        bookChap.appendChild(newBtn);
+        newBtn.textContent = '\u274C';
+        list.appendChild(bookChap);
+
+        newBtn.addEventListener('click', () => {
+          list.removeChild(bookChap);
+        });
+
+        input.focus();
+    });
