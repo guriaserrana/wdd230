18af93dc434b74934870089823b6c1ba47b94218
guriaserrana <ap.of26@gmail.com>
2022-06-22 00:58:46 -0300

    discover and lazy corrected

--- lesson7/js/scripts.js
+++ lesson7/js/scripts.js
@@ -1,24 +1,27 @@
-const targets = document.querySelectorAll('img');
+document.addEventListener("DOMContentLoaded", function() {
+  let lazyloadImages = document.querySelectorAll("img.lazy-load");
+  let lazyloadThrottleTimeout;
 
-const lazyLoad = target => {
-  const io = new IntersectionObserver((entries, observer) => {
-    console.log(entries)
-    entries.forEach(entry => {
-      
-
-      if (entry.isIntersecting) {
-        const img = entry.target;
-        const src = img.getAttribute('data-lazy');
-
-        img.setAttribute('src', src);
-        img.classList.add('fade');
-
-        observer.disconnect();
+  function lazyload() {
+    if(lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }
+    lazyloadThrottleTimeout = setTimeout(function() {
+      let scrollTop = window.pageYOffset;
+      lazyloadImages.forEach(function(img) {
+        if(img.offsetTop < (window.innerHeight + scrollTop)) {
+          img.src = img.dataset.src;
+          img.classList.remove('lazy');
+        }
+      });
+      if(lazyloadImages.length == 0) {
+        document.removeEventListener("scroll", lazyload);
+        window.removeEventListener("resize", lazyload);
+        window.removeEventListener("orientationChange", lazyload);
       }
-    });
-  });
-
-  io.observe(target)
-};
-
-targets.forEach(lazyLoad);
+    }, 20);
+  }
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
+});
