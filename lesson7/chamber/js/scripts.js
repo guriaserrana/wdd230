d6b9fed26ca4b37f7cbd24b9542120df7016502c
guriaserrana <ap.of26@gmail.com>
2022-06-22 22:04:55 -0300

    lesson7 corrected

--- lesson7/chamber/js/scripts.js
+++ lesson7/chamber/js/scripts.js
@@ -37,4 +37,44 @@ if(tempNumber<=50 && speedNumber >3) {
 
 
 
+//LAST VISIT
+const visitDisplay = document.querySelector('#lastVisit')
+const mInDay = 1000 * 60 * 60 * 24;
+const today = Number(Date.now());
+console.log(`millisecond in a day is ${mInDay}`);
+
+let lastVisit = Number(window.localStorage.getItem("visit-is"));
+localStorage.setItem("visit-is", today);
+let difference = Math.round((now - lastVisit)/mInDay);
+console.log(difference)
+
+if (difference == 0) {
+    visitDisplay.textContent = `Thank you for visiting us!`
+} else {
+    visitDisplay.textContent = (`Days since last visit ${difference}`);
+}
+
+lastVisit++;
+localStorage.setItem("visits-ls", lastVisit);
+
+//OVERLAY
+
+    const todayy = new Date();
+    console.log(todayy);
+
+    const dayNumber = todayy.getDay();
+    console.log(dayNumber);
+
+    const element = document.getElementById("message");
+
+    if (dayNumber == 1) {
+        element.classList.add("showme");        
+    } if (dayNumber == 3) {
+        element.classList.add("showme");
+    } else {
+        element.classList.add("hideme");        
+    }
+
+
+
 
