449f01f3132fa5ffa41002d9b1865ba62f4d1b65
guriaserrana <ap.of26@gmail.com>
2022-06-23 21:26:40 -0300

    Join added

---
+++ lesson8/chamber/js/scripts.js
@@ -0,0 +1,80 @@
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+// set current year and time/date in footer
+const currentDate = new Date();
+document.querySelector('#year').textContent = currentDate.getFullYear();
+document.querySelector('#date').textContent = document.lastModified;
+
+
+//menu
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+//weather summary 
+const tempNumber = parseFloat(document.getElementById("temp").textContent);
+const speedNumber = parseFloat(document.getElementById("speed").textContent);
+
+let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, 
+0.16)) + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
+
+windchill = Math.round(windchill);
+
+if(tempNumber<=50 && speedNumber >3) {
+    document.getElementById("chill").textContent = "Wind Chill is" + " " + windchill + "\xB0F";
+
+} else {
+    document.getElementById("chill").textContent = "No Wind Chill Today";
+}
+
+
+
+//LAST VISIT
+const visitDisplay = document.querySelector('#lastVisit')
+const mInDay = 1000 * 60 * 60 * 24;
+const today = Number(Date.now());
+console.log(`millisecond in a day is ${mInDay}`);
+
+let lastVisit = Number(window.localStorage.getItem("visit-is"));
+localStorage.setItem("visit-is", today);
+let difference = Math.round((now - lastVisit)/mInDay);
+console.log(difference)
+
+if (difference == 0) {
+    visitDisplay.textContent = `Thank you for visiting us!`
+} else {
+    visitDisplay.textContent = (`Days since last visit ${difference}`);
+}
+
+lastVisit++;
+localStorage.setItem("visits-ls", lastVisit);
+
+//OVERLAY
+
+    const todayy = new Date();
+    console.log(todayy);
+
+    const dayNumber = todayy.getDay();
+    console.log(dayNumber);
+
+    const element = document.getElementById("message");
+
+    if (dayNumber == 1) {
+        element.classList.add("showme");        
+    } if (dayNumber == 3) {
+        element.classList.add("showme");
+    } else {
+        element.classList.add("hideme");        
+    }
+
+
+
+
