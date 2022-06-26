444988af8367adc757ee055391c5e2b7fa8e2ab9
guriaserrana <ap.of26@gmail.com>
2022-06-26 02:18:09 -0300

    lesson 10 chamber

---
+++ lesson10/chamber/js/directory.js
@@ -0,0 +1,68 @@
+const requestURL = 'https://raw.githubusercontent.com/guriaserrana/wdd230/main/lesson9/chamber/data/data.json';
+const cards = document.querySelector('.cards2');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const companies = jsonObject['companies'];
+    companies.forEach(displayCompanies);
+  });
+
+function displayCompanies(company) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let website = document.createElement('a');  //adding website link 
+    let portrait = document.createElement('img');
+  
+    
+    h2.textContent = company.company;     
+    p1.textContent = company.address;   
+    p2.textContent = `phone: ${company.phonenumber}`; 
+    //adding website link
+    website.textContent = company.site;   
+    website.setAttribute('href', company.site);
+    website.setAttribute('target', '_blank');
+
+
+    // Build the image attributes by using the setAttribute method for the src, alt,
+    // and loading attribute values. (Fill in the blank with the appropriate variable).
+    portrait.setAttribute('src', company.imageurl);
+    portrait.setAttribute('alt', 'logo for' + " " + company.name);
+    portrait.setAttribute('width', 200)
+    portrait.setAttribute('loading', 'lazy');
+  
+    // Add/append the section(card) with the h2 element
+    card.appendChild(portrait);
+    card.appendChild(h2);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(website);  
+    
+  
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards2').appendChild(card);
+  };
+
+    const gridbutton = document.querySelector("#grid");
+    const listbutton = document.querySelector("#list");
+    const display = document.querySelector(".cards2");  
+    
+    gridbutton.addEventListener("click", () => {
+        
+        display.classList.add("grid");
+        display.classList.remove("list");
+    });
+    
+    listbutton.addEventListener("click", showList);
+    
+    function showList() {
+        display.classList.add("list");
+        display.classList.remove("grid");
+    }
+    
---
+++ lesson10/chamber/js/weather.js
@@ -0,0 +1,31 @@
+    // select HTML elements to edit
+    const currentTemp = document.querySelector('#current-temp');
+    const tempMax = document.querySelector('#max');
+    const tempMin = document.querySelector('#min');
+    // const weatherIcon = document.querySelector('#weather-icon');
+    const captionDesc = document.querySelector('figcaption');
+    const windSpeed = document.querySelector('#speed');
+    const windChill = document.querySelector('#wch');
+
+    const url = "//api.openweathermap.org/data/2.5/weather?id=3458930&appid=02b739bf12eedc884c1f3c1ef6f4146b&units=imperial";
+    fetch(url)
+    .then((response) => response.json())
+    .then((weatherInfo) => {
+        console.log(weatherInfo); // this is temporary for development only
+
+        currentTemp.innerHTML = `<strong>${weatherInfo.main.temp.toFixed(0)}</strong>`;
+        tempMax.innerHTML = `<strong>${weatherInfo.main.temp_max.toFixed(0)}</strong>`;
+        tempMin.innerHTML = `<strong>${weatherInfo.main.temp_min.toFixed(0)}</strong>`;
+        windSpeed.innerHTML = weatherInfo.wind.speed;
+        captionDesc.innerHTML = weatherInfo.weather.description;
+    
+
+        // const iconsrc = `https://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`;
+        const desc = weatherInfo.weather[0].description;
+
+        // weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
+        captionDesc.textContent = desc;
+
+
+
+    });
